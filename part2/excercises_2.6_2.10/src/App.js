5f86846ec837cbe6780227c0c486ab7efba74afd
Alfaruga <alfrauga@gmail.com>
2022-11-01 09:51:14 -0600

    2.5 to 2.7 done

--- part2/excercises_2.6_2.10/src/App.js
+++ part2/excercises_2.6_2.10/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const equals = (objectA, objectB) => {
+    const a = Object.getOwnPropertyNames(objectA);
+    const b = Object.getOwnPropertyNames(objectB);
+    if (a.length !== b.length) return false;
+    const hasAllKeys = a.every((value) => !!b.find((v) => v === value));
+    if (!hasAllKeys) return false;
+    for (const key of a) if (objectA[key] !== objectB[key]) return false;
+    return true;
+  };
+  
+  const inputHandler = (event) => {
+    setNewName(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const alreadyAdded = persons.some((person) => equals(person, { name: newName }));
+
+    if (alreadyAdded) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat({ name: newName }));
+    }
+    setNewName("");
+  };
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={submitHandler}>
+        <div>
+          name: <input value={newName} onChange={inputHandler} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        return <p key={person.name}>{person.name}</p>;
+      })}
     </div>
   );
-}
+};
 
 export default App;
