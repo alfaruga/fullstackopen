767e670a637f709615cef2c2ec7ebead624c4ad9
Alfaruga <alfrauga@gmail.com>
2022-11-10 12:46:23 -0600

    add useEffect hook, filtered and list length

--- part2/excercises_2.12_2.14/src/App.js
+++ part2/excercises_2.12_2.14/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [countries, setCountries] = useState([]);
+
+  const searchHandler = (event) => {
+    setSearch(event.target.value);
+  };
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      const restCountries = response.data.map(country => country.name.common);
+      setCountries(restCountries);
+    });
+  }, []);
+
+  var content = "";
+  const filteredCountries = countries
+    .filter((value) => {
+      return value.toLowerCase().includes(search.toLowerCase());
+    })
+    .map((value) => {
+      return <p key={value + 1*Math.random(1)}>{value}</p>;
+    });
+
+  if(search === "" ){
+    content = "Countries will be displayed in here!"
+  }
+    else if (filteredCountries.length > 10) {
+    content = "Too many matches, specify another filter";
+  } else {
+    content = filteredCountries;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>public stuff</h1>
+      <input value={search} onChange={searchHandler}></input>
+      debugger, state "search": {search}
+      <h3>Countries:</h3>
+      {content}
     </div>
   );
 }
