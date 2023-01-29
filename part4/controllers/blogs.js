806ee541ef65d55edc78cee64e7e0341b1f7c162
Alfaruga <alfrauga@gmail.com>
2023-01-29 11:36:07 -0600

    First two exercises, make a new npm project and refractor! good stuff!

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,34 @@
+const bloggsRouter = require("express").Router();
+const Blogg = require("../models/blog");
+
+bloggsRouter.get("/", (request, response, next) => {
+  Blogg.find({})
+    .then((blogg) => {
+      if (blogg) {
+        response.json(blogg);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+bloggsRouter.post("/", (request, response, next) => {
+  const body = request.body;
+  const blogg = new Blogg({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+
+  blogg
+  .save()
+  .then(savedBlogg=>{
+    response.status(201).json(savedBlogg)
+  })
+  .catch(error=>next(error))
+});
+
+
+module.exports = bloggsRouter
