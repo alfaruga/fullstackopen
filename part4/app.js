73d77655ae916c8f9d6bef0f28d6a733a41a32e3
Alfaruga <alfrauga@gmail.com>
2023-03-16 21:15:19 -0600

    Added token based authentication exercise 4.18

--- part4/app.js
+++ part4/app.js
@@ -10,7 +10,7 @@ const middleware = require("./utils/middleware");
 
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
-
+const loginRouter = require('./controllers/login')
 mongoose.set("strictQuery", false);
 
 logger.info("connecting to", config.MONGODB_URI);
@@ -31,7 +31,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
-
+app.use("/api/login", loginRouter)
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
