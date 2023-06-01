84cd380224f2d9a9d16be0728376d2b789522bcb
Alfaruga <alfrauga@gmail.com>
2023-06-01 08:44:10 -0600

    UPLOADED exercise 7.8, which is a service-like hook, that handles the server comunication

--- part7/country/src/App.js
+++ part7/country/src/App.js
@@ -1,80 +1,81 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const useField = (type) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("");
 
   const onChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
   return {
     type,
     value,
-    onChange
-  }
-}
+    onChange,
+  };
+};
 
-const useCountry =  (name) => {
-  const [country, setCountry] = useState('')
-  
-  useEffect(()=>{
-    if(!name){
-      return 
+const useCountry = (name) => {
+  const [country, setCountry] = useState("");
+
+  useEffect(() => {
+    if (!name) {
+      return;
     }
-    console.log('effect ran')
     axios
-    .get('https://studies.cs.helsinki.fi/restcountries/api/name/'+name)
-    .then((response)=>{
-console.log(response.data)
-const country = {'data': {name:response.data.name.common, capital: response.data.capital[0], population:response.data.population, flag:response.data.flags.svg}, 'found': true}
-setCountry(country)
-  
-    }).catch((e)=>{
-      console.log("reaches error")
-      const country = {'found': false}
-      setCountry(country)
-    })
-     
-    
-}, [name])
-console.log('last log before sending country', country)
-return country
-}
+      .get("https://studies.cs.helsinki.fi/restcountries/api/name/" + name)
+      .then((response) => {
+        const country = {
+          data: {
+            name: response.data.name.common,
+            capital: response.data.capital[0],
+            population: response.data.population,
+            flag: response.data.flags.svg,
+          },
+          found: true,
+        };
+        setCountry(country);
+      })
+      .catch((e) => {
+        const country = { found: false };
+        setCountry(country);
+      });
+  }, [name]);
+  return country;
+};
 
 const Country = ({ country }) => {
   if (!country) {
-    return null
+    return null;
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>;
   }
 
   return (
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
-const country = useCountry(name)
+  const nameInput = useField("text");
+  const [name, setName] = useState("");
+  const country = useCountry(name);
 
   const fetch = (e) => {
-    e.preventDefault()
-    setName(nameInput.value)
-    
-  }
+    e.preventDefault();
+    setName(nameInput.value);
+  };
 
   return (
     <div>
@@ -85,7 +86,7 @@ const country = useCountry(name)
 
       <Country country={country} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
